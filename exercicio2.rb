270b0a950b5e34d686183de878f3882a281f98ae
Daniel Filho <daniel.acfilho@gmail.com>
2014-08-15 03:35:35 -0300

    Finalizando o exercício

--- exercicio2.rb
+++ exercicio2.rb
@@ -2,10 +2,12 @@ require './caixa'
 require './cliente'
 
 class Banco
-  attr_accessor :caixas, :clientes, :clientes_atrasados, :hora_atendimento
+  attr_accessor :caixas, :clientes, :clientes_atrasados, :hora_atendimento_agencia
 
   def initialize(input)
     input = input.split("\n")
+    @clientes_atrasados = 0
+    @hora_atendimento_agencia = 0
 
     @caixas = input.shift.split().first.to_i
     @caixas = (1..@caixas).map do
@@ -15,20 +17,43 @@ class Banco
     @clientes = input.map do |cliente_info|
       c = Cliente.new(*cliente_info.split(" ").map(&:to_i))
     end
-
-    @clientes_atrasados = 0
-    @hora_atendimento = 0
   end
 
-  def atendimentos_atrasados
-    until @clientes.empty? do
-      buscar_cliente
+  def iniciar_expediente
+    until clientes.empty? do
+      clientes.each do |cliente|
+        if(hora_atendimento_agencia >= cliente.chegada)
+          caixas.each do |caixa|
+            if(hora_atendimento_agencia >= caixa.disponivel_em)
+              caixa.disponivel_em = cliente.duracao_atendimento + hora_atendimento_agencia
+
+              atendimentos_atrasados(cliente.chegada)
+              atendimento_concluido
+              break
+            end
+          end
+        end
+      end
+
+      self.hora_atendimento_agencia += 1
     end
   end
 
-  def buscar_cliente
-    @clientes.each do |cliente|
-      return cliente if @hora_atendimento >= cliente.chegada
-    end
+  def atendimentos_atrasados(chegada)
+    self.clientes_atrasados += 1 if (hora_atendimento_agencia - chegada > 20)
+  end
+
+  def atendimento_concluido
+    self.clientes.shift
   end
 end
+
+# Exibindo o resultado
+
+resultado1 = Banco.new(File.read("input1.txt"))
+resultado1.iniciar_expediente
+puts "Entrada arquivo ./input1.txt - Saida: #{resultado1.clientes_atrasados}"
+
+resultado2 = Banco.new(File.read("input.txt"))
+resultado2.iniciar_expediente
+puts "Entrada arquivo ./input.txt - Saida: #{resultado2.clientes_atrasados}"
--- spec/exercicio2_spec.rb
+++ spec/exercicio2_spec.rb
@@ -39,7 +39,7 @@ describe Banco do
     end
 
     it "deveria comecar o atendimento no banco como zero" do
-      expect(subject.hora_atendimento).to eql(0)
+      expect(subject.hora_atendimento_agencia).to eql(0)
     end
 
     it "deveria comecar com o numero de clientes atrasados zero" do
@@ -47,16 +47,66 @@ describe Banco do
     end
   end
 
-  describe "#buscar_cliente" do
-    context "retornando o cliente" do
-      before do
-        subject.hora_atendimento = 10
-        subject.clientes.first.chegada = 10
+  describe "#iniciar_expediente" do
+    let :input do
+"3 16
+0 10"
+    end
+
+    context "com apenas 1 cliente" do
+      subject do
+        Banco.new(input)
       end
 
-      it "caso esteja no seu horario" do
-        expect(subject.buscar_cliente).to eql(subject.clientes.first)
+      before do
+        subject.iniciar_expediente
       end
+
+      it "deveria ter a hora_atendimento_agencia 1" do
+        expect(subject.hora_atendimento_agencia).to eql(1)
+      end
+
+      it "deveria ter dois caixas livres" do
+        expect(subject.caixas[1].disponivel_em).to eql(0)
+        expect(subject.caixas[2].disponivel_em).to eql(0)
+      end
+
+      it "deveria ter um caixa ocupado" do
+        expect(subject.caixas[0].disponivel_em).to eql(10)
+      end
+    end
+  end
+
+  describe "#atendimentos_atrasados" do
+    context "cliente atrasado" do
+      before do
+        subject.hora_atendimento_agencia = 30
+        expect(subject.clientes_atrasados).to eql(0)
+      end
+
+      it "deveria adicionar mais um cliente a lista" do
+        subject.atendimentos_atrasados(1)
+        expect(subject.clientes_atrasados).to eql(1)
+      end
+    end
+
+    context "cliente nao atrasado" do
+      before do
+        subject.hora_atendimento_agencia = 30
+        expect(subject.clientes_atrasados).to eql(0)
+      end
+
+      it "deveria manter a lista de clientes atrasados limpa" do
+        subject.atendimentos_atrasados(10)
+        expect(subject.clientes_atrasados).to eql(0)
+      end
+    end
+  end
+
+  describe "#atendimento_concluido" do
+    it "deveria remover o primeiro cliente da fila" do
+      subject.atendimento_concluido
+      expect(subject.clientes.size).to eql(15)
     end
   end
 end
