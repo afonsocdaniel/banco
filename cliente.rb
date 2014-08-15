3dad6a6ffa38115194a5c1e85cb8ebd0bffa275d
Daniel Filho <daniel.acfilho@gmail.com>
2014-08-15 01:49:17 -0300

    Cliente agora pode ter seus valores configurado na inicialização

--- cliente.rb
+++ cliente.rb
@@ -1,8 +1,8 @@
 class Cliente
   attr_accessor :chegada, :duracao_atendimento
 
-  def initialize
-    @chegada = 0
-    @duracao_atendimento = 0
+  def initialize(chegada, duracao_atendimento)
+    @chegada = chegada
+    @duracao_atendimento = duracao_atendimento
   end
 end
--- spec/cliente_spec.rb
+++ spec/cliente_spec.rb
@@ -3,22 +3,22 @@ require './cliente'
 
 describe Cliente do
   subject do
-    Cliente.new
+    Cliente.new(10, 30)
   end
 
   describe "#initialize" do
-    it "deveria configurar o valor default para o atributo 'chegada' como zero" do
-      expect(subject.chegada).to eql(0)
+    it "deveria poder configurar o valor para o atributo 'chegada'" do
+      expect(subject.chegada).to eql(10)
     end
 
     it "deveria configurar o valor default para o atributo 'duracao_atendimento' como zero" do
-      expect(subject.duracao_atendimento).to eql(0)
+      expect(subject.duracao_atendimento).to eql(30)
     end
   end
 
   describe "#chegada" do
     it "deveria retornar o valor setado" do
-      expect(subject.chegada).to eql(0)
+      expect(subject.chegada).to eql(10)
     end
 
     it "deveria poder setar" do
@@ -29,7 +29,7 @@ describe Cliente do
 
   describe "#duracao_atendimento" do
     it "deveria retornar o valor setado" do
-      expect(subject.duracao_atendimento).to eql(0)
+      expect(subject.duracao_atendimento).to eql(30)
     end
 
     it "deveria poder setar" do
