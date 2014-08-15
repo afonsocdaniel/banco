69835628dc9dc19b3e94a8e167f7e18c9150e847
Daniel Filho <daniel.acfilho@gmail.com>
2014-08-15 01:42:23 -0300

    Configurando o valor default da Caixa

--- caixa.rb
+++ caixa.rb
@@ -1,3 +1,7 @@
 class Caixa
   attr_accessor :disponivel_em
+
+  def initialize
+    @disponivel_em = 0
+  end
 end
--- spec/caixa_spec.rb
+++ spec/caixa_spec.rb
@@ -6,14 +6,18 @@ describe Caixa do
     Caixa.new
   end
 
+  describe "#initialize" do
+    it "deveria configurar o valor default do atributo 'disponivel_em' como zero" do
+      expect(subject.disponivel_em).to eql(0)
+    end
+  end
+
   describe "#disponivel_em" do
     it "deveria retornar o valor setado" do
-      subject.disponivel_em = 10
-      expect(subject.disponivel_em).to eql(10)
+      expect(subject.disponivel_em).to eql(0)
     end
 
     it "deveria poder setar" do
-      expect(subject.disponivel_em).to eql(nil)
       subject.disponivel_em = 3
       expect(subject.disponivel_em).to eql(3)
     end
